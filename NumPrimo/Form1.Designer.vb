7b2eeb525ff2ef8de4208a1d0471b2d5ddc8b046
luis_ <luis_@DELL>
2023-04-23 21:51:39 -0600

    Agregar archivos de proyecto.

---
+++ NumPrimo/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        BtnCalcular = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        LblPrimo = New Label()
+        SuspendLayout()
+        ' 
+        ' BtnCalcular
+        ' 
+        BtnCalcular.Location = New Point(152, 120)
+        BtnCalcular.Name = "BtnCalcular"
+        BtnCalcular.Size = New Size(112, 34)
+        BtnCalcular.TabIndex = 1
+        BtnCalcular.Text = "Calcular"
+        BtnCalcular.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(197, 29)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 0
+        TextBox1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(28, 32)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(163, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Escribe un numero:"
+        ' 
+        ' LblPrimo
+        ' 
+        LblPrimo.Location = New Point(28, 68)
+        LblPrimo.Name = "LblPrimo"
+        LblPrimo.Size = New Size(319, 33)
+        LblPrimo.TabIndex = 3
+        LblPrimo.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(424, 194)
+        Controls.Add(LblPrimo)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(BtnCalcular)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Numeros Primos"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents BtnCalcular As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LblPrimo As Label
+End Class
---
+++ NumPrimo/Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Reflection
+Imports System.Reflection.Emit
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnCalcular.Click
+        Try
+            validarPrimo()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub validarPrimo()
+        Try
+            Dim numero As Integer = Val(TextBox1.Text)
+            Dim esPrimo As Boolean = True
+            If numero <= 1 Then
+                esPrimo = False
+            Else
+                For i As Integer = 2 To Math.Sqrt(numero)
+                    If numero Mod i = 0 Then
+                        esPrimo = False
+                        Exit For
+                    End If
+                Next
+            End If
+
+            If esPrimo Then
+                LblPrimo.Text = $"{numero} Si, es primo"
+            Else
+                LblPrimo.Text = $"{numero} No, es primo"
+            End If
+            TextBox1.Clear()
+            TextBox1.Focus()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        Try
+            If e.KeyCode = Keys.Enter Then
+                validarPrimo()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
